d493d6de1dc4f6dac65edcde2ff011e84f9cd319
Josh Wotman <joshwotman@gmail.com>
2021-03-12 13:17:21 -0500

    Added initial state of nav and logo to header for mobile.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import CustomNav from './nav.component';
 
 
 
@@ -10,13 +10,31 @@ const StyledHeader = styled.header`
     background-image: url('/image-hero-mobile.jpg');
     background-repeat:no-repeat;
     background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
 
+`;
+
+const HeaderElementContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 3.2rem 2.4rem 0 2.4rem;
+    height: 2rem;
+`;
+
+const CompanyLogo = styled.div`
+    background-image: url('/logo.svg');
+    width: 12.8rem;
 `;
 
 const CustomHeader=()=> (
     <StyledHeader>
-        
-      
+        <HeaderElementContainer>
+            <CompanyLogo/>
+            <CustomNav/>
+        </HeaderElementContainer> 
     </StyledHeader>
     
   );
