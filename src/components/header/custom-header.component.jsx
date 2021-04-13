ae5eb72b08cca96e8fee9b7dbe3741eba981b2ff
Josh Wotman <joshwotman@gmail.com>
2021-04-13 13:46:59 -0400

    added partial overlay for mobile menu

--- src/components/header/custom-header.component.jsx
+++ src/components/header/custom-header.component.jsx
@@ -18,14 +18,19 @@ const StyledHeader = styled.header`
 `;
 
 const PartialOverlay = styled.div`
-
-
-  display: ${props => props.overlayHidden ? 'none' : 'block'}; /* Hidden by default */
+  position: fixed; /* Sit on top of the page content */
+  display: ${props => props.overlayHidden ? 'none' : 'flex'}; /* Hidden by default */
   width: 100%; /* Full width (cover the whole page) */
   height: 100%; /* Full height (cover the whole page) */
-  
+  top: 27rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
   background-color: rgba(0,0,0,0.5); /* Black background with opacity */
-  
+  z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+  cursor: pointer; /* Add a pointer on hover */
+  overflow-y: scroll;
+  flex-direction: column;  
 `;
 
 const TopHeaderElementContainer = styled.div`
