b5e2add3e6931e0fa2264db42fa6feb2267d4b3e
Josh Wotman <joshwotman@gmail.com>
2021-04-13 13:17:04 -0400

    added mobile menu (without partial overlay below menu) and fixed a couple of style bugs

---
+++ src/components/header/custom-header.component.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components';
+import NavIcon from './nav-icon';
+import {ReactComponent as Logo} from '../../images/logo.svg';
+
+
+
+const StyledHeader = styled.header`
+    height: 30rem;
+    width: 100%;
+    background-image: url('/image-hero-mobile.jpg');
+    background-repeat:no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+`;
+
+const PartialOverlay = styled.div`
+
+
+  display: ${props => props.overlayHidden ? 'none' : 'block'}; /* Hidden by default */
+  width: 100%; /* Full width (cover the whole page) */
+  height: 100%; /* Full height (cover the whole page) */
+  
+  background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+  
+`;
+
+const TopHeaderElementContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 3.2rem 2.4rem 0 2.4rem;
+    height: 2rem;
+    background-color: rgba(255,255,255,0.0);
+`;
+
+
+const NavList = styled.ul`
+    position: relative;
+    background-color: white;
+    display: ${props => props.menuOpen ? 'flex' : 'none'};
+    flex-direction: column;
+    margin: 8.8rem 2.4rem 0 2.4rem;
+`;
+
+const NavItem = styled.li`
+   display: flex;
+   justify-content: flex-start;
+   align-items: center;
+   border: .1rem solid  lightgray ;
+   color: black;
+   font-size: 1.8rem;
+   padding: 1.2rem 0 1.2rem 2.4rem ;
+
+`;
+
+
+class CustomHeader extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false
+        };
+        this.toggleMenuOpen = this.toggleMenuOpen.bind(this);
+    }
+
+    toggleMenuOpen(){
+            
+        this.setState({menuOpen: !this.state.menuOpen});
+        
+    }
+    
+    render(){
+        return <StyledHeader>
+                   
+                    <TopHeaderElementContainer>
+                        <Logo/>
+                        <NavIcon menuOpen={this.state.menuOpen} handleClick={this.toggleMenuOpen}/>
+                    </TopHeaderElementContainer> 
+                    
+                        <NavList menuOpen={this.state.menuOpen}>
+                            <NavItem>About</NavItem>
+                            <NavItem>Discover</NavItem>
+                            <NavItem>Get Started</NavItem>
+                        </NavList>
+                        <PartialOverlay overlayHidden={!this.state.menuOpen} />
+                    
+                </StyledHeader>
+    }    
+};
+
+export default CustomHeader;
---
+++ src/components/header/nav-icon.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import {ReactComponent as IconHamburger} from '../../images/icon-hamburger.svg';
+import {ReactComponent as IconCloseModal} from '../../images/icon-close-modal-white.svg';
+
+
+
+const Icon = styled.div`
+
+    display: flex;
+    background-color: rgba(255,255,255,0.0);
+    flex-direction: column;
+
+
+`;
+
+const IconHamburgerWrapper = styled.div`
+
+display: ${props => props.menuOpen ? 'none' : 'block'};
+background-color: rgba(255,255,255,0.0);
+`;
+
+const IconCloseModalWrapper = styled.div`
+display: ${props => props.menuOpen ? 'block' : 'none'};
+background-color: rgba(255,255,255,0.0);
+`;
+
+
+
+
+
+const CustomNav = ({handleClick, menuOpen}) => (
+        <Icon>
+            <IconHamburgerWrapper menuOpen={menuOpen} onClick={handleClick}><IconHamburger  /> </IconHamburgerWrapper>
+            <IconCloseModalWrapper menuOpen={menuOpen} onClick={handleClick}><IconCloseModal  /> </IconCloseModalWrapper>
+        </Icon>
+);
+
+
+export default CustomNav;
