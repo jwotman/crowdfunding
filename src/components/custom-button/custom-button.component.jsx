1eb397cc6b5d26032334ea4f900ce78417a49edf
Josh Wotman <joshwotman@gmail.com>
2021-05-28 15:48:25 -0400

    cleaned up commented code

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -16,7 +16,7 @@ export const StyledButton = styled.button`
   cursor: pointer;
   width: 21.4rem;
   height: 5.6rem;
-  margin: .4rem .2rem;
+  //margin: .4rem .2rem;
   border-radius: 3.3rem;
   :hover{
     background-color: #147A73;
