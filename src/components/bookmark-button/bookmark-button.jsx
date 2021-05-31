0451a2a1abf6bc8a9902b134db1dd9afb94a44e1
Josh Wotman <joshwotman@gmail.com>
2021-05-31 13:45:06 -0400

    added the hover effect from figma design

--- src/components/bookmark-button/bookmark-button.jsx
+++ src/components/bookmark-button/bookmark-button.jsx
@@ -9,39 +9,52 @@ import { bookmarkURL } from '../../redux/ui_control/ui_control.actions';
 import { selectProjectBookmarked } from '../../redux/ui_control/ui_control.selectors';
 import bookmarkImage from '../../images/icon-bookmark.svg';
 import bookmarkActiveImage from '../../images/icon-bookmark-active.svg'
+import bookmarkHoverImage from '../../images/icon-bookmark-hover.svg';
 
 const BookmarkIcon = styled.div.attrs(props => ({
-    role: "image", "aria-label": "Bookmark"
+    role: "image", "aria-label": "Bookmark", className: "bookmark-icon"
 }))`
 
-background-image: ${props => props.projectBookmarked ?  'url("' + bookmarkActiveImage + '")' : 'url("' + bookmarkImage + '")'};
+    background-image: ${props => props.projectBookmarked ?  'url("' + bookmarkActiveImage + '")' : 'url("' + bookmarkImage + '")'};
     background-repeat:no-repeat;
     background-size: cover;
     border: none;
     border-radius: 50%;
     height: 5.6rem;
     width: 5.6rem;
-    transform: translateY(.4rem);
+    z-index: 2;
+
+    :hover {
+        background-image: url("${bookmarkHoverImage}");
+    }
+    //transform: translateY(2rem);
 `;
 
 const ButtonContainer = styled.div`
     display: flex;
     justify-content: space-between;
+    :hover > ${BookmarkIcon}{
+        background-image: url("${bookmarkHoverImage}");
+    }
 `;
 
 const DesktopButton = styled(StyledButton)`
     display: none;
     width: 17.4rem;
     background-color: #f2f2f2;
+    
     color: ${props => props.projectBookmarked ? '#147A73' : '#7A7A7A'};
     padding-left: 5rem;
+    
     @media (min-width: 525px) {
-        display: initial;
+        display: block;
         margin-left: -5rem;
         margin-right: 1rem;
-    }
-    
-    
+        :hover {
+            background-color: #f2f2f2;
+        }
+        
+    }   
 `
 
 const BookmarkButton=({children, projectBookmarked,bookmarkProject})=> (
